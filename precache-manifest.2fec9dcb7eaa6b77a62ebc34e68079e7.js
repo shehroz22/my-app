551844aaa246dbeca678a20e5e26c513f132570e
shehroz22 <94064174+shehroz22@users.noreply.github.com>
2022-07-06 23:36:41 +0800

    Updates

---
+++ precache-manifest.2fec9dcb7eaa6b77a62ebc34e68079e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1ae035c491d1eb01566",
+    "url": "/shehroz22/my-app/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "b1ae035c491d1eb01566",
+    "url": "/shehroz22/my-app/static/js/main.33e5e85a.chunk.js"
+  },
+  {
+    "revision": "1c1ae012e7097b929541",
+    "url": "/shehroz22/my-app/static/js/runtime~main.23cac146.js"
+  },
+  {
+    "revision": "bb21798b792c99b8ae9a",
+    "url": "/shehroz22/my-app/static/js/2.8b9ca286.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/shehroz22/my-app/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "18539fd6b9a4b697ce78d42a03f6d4ae",
+    "url": "/shehroz22/my-app/index.html"
+  }
+];
