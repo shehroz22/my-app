1076df5ddbc2d60e0cecfd4139a759a37a4e492f
shehroz22 <94064174+shehroz22@users.noreply.github.com>
2022-07-08 00:21:01 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shehroz22/my-app/precache-manifest.2fec9dcb7eaa6b77a62ebc34e68079e7.js"
+  "/precache-manifest.f077f1ee296e14d73c7ccaa9b0247389.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/shehroz22/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
